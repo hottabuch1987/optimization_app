5891770cc94bcdde531d4d1a200230c44e9d22bc
hottabych <varvar1987a@mail.ru>
2023-06-18 11:50:34 +0300

    Day 2 - 2: User extended AbstractBaseUser

---
+++ service/clients/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+
+
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email, password, **extra_fields):
+
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    def create_superuser(self, email, password, **extra_fields):
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+
+# модель пользователя
+class User(AbstractBaseUser, PermissionsMixin):
+
+    GENDER_TYPES = (
+        ("women", 'женщина'),
+        ("men", 'мужчина'),
+    )
+    email = models.EmailField("Email", unique=True)
+    name = models.CharField("Имя", max_length=255, blank=True, default='')
+    avatar = models.ImageField("Фото", upload_to='avatars', blank=True, null=True)
+    gender = models.CharField(choices=GENDER_TYPES, max_length=10)
+    is_active = models.BooleanField("Активный", default=True)
+    is_superuser = models.BooleanField("Суперпользователь", default=False)
+    is_staff = models.BooleanField("Администратор", default=False)
+    date_joined = models.DateTimeField("Дата регистрации", default=timezone.now)
+    last_login = models.DateTimeField("Последний визит", blank=True, null=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def get_avatar(self):
+        if self.avatar:
+            return 'http://127.0.0.1:8000' + self.avatar.url
+        else:
+            return 'https://loremflickr.com/320/240'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f'{self.email}'
+
