1cc06b1c7b46050d983264cb3a5ea8ebdaccfff8
hottabych <varvar1987a@mail.ru>
2023-06-25 17:08:30 +0300

    Day 5 - аннотация на уровне базы

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -14,7 +14,13 @@ class SubscriptionSerializer(serializers.ModelSerializer):
 
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.email')
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, instance):      #формула расчета
+        return instance.price
+        # return (instance.service.full_price -
+        #         instance.service.full_price * (instance.plan.discount_percent / 100))
 
     class Meta:
         model = Subscription
-        fields = ('id', 'date_joined', 'plan_id', 'client_name', 'email', 'created_at_formatted', 'plan')
+        fields = ('id', 'date_joined', 'plan_id', 'client_name', 'email', 'created_at_formatted', 'plan', 'price')
--- service/services/views.py
+++ service/services/views.py
@@ -1,14 +1,15 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import User
-from .models import Subscription
+from .models import Subscription, Plan
 from .serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related('plan',
         Prefetch('client', queryset=User.objects.all().only('company_name', 'email'))
-    )
+    ).annotate(price=F('service__full_price') -
+                     F('service__full_price') * F('service__full_price') / 100.00 )
     serializer_class = SubscriptionSerializer
