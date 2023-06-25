6f0fc530dc3f39cf0a09ea982eb41a08f266a230
hottabych <varvar1987a@mail.ru>
2023-06-25 16:16:10 +0300

    Day 4 - Оптимизация SQL: prefetch_related ('plan')Prefetch.only

--- service/services/views.py
+++ service/services/views.py
@@ -1,10 +1,14 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
+from clients.models import User
 from .models import Subscription
 from .serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all().prefetch_related('client')
+    queryset = Subscription.objects.all().prefetch_related('plan',
+        Prefetch('client', queryset=User.objects.all().only('company_name', 'email'))
+    )
     serializer_class = SubscriptionSerializer
